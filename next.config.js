ceaea00e66ac90c078f26ac25ceb2d86487a5f8c
JEREMIA-SUNNY <you@example.com>
2022-11-16 11:37:01 +0530

    new changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: ["assets.acme.com"],
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
